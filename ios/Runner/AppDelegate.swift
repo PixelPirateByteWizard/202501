fe775505417929dcd07ffa51e49f1aa29d1a607c
sharavanb <sharavanbagga@icloud.com>
2025-04-05 18:54:57 +0800

    “0233”

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,7 @@
 import Flutter
 import UIKit
-import EnyxorameaRegister
+// import EnyxorameaRegister
 import AppTrackingTransparency
-
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,54 +9,20 @@ import AppTrackingTransparency
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      
-      
-      CheckMediocreLoaderHelper.syncSingleVectorHandler();
-      CheckMediocreLoaderHelper.revisitMissedRightTarget();
-      CheckMediocreLoaderHelper.resumeIntermediateOperationProtocol();
-      CheckMediocreLoaderHelper.retrieveHardInterpolationImplement();
-      CheckMediocreLoaderHelper.trainPermanentIndexFactory();
-      CheckMediocreLoaderHelper.pausePrimaryNumberCreator();
-      CheckMediocreLoaderHelper.divideIterativeParameterProtocol();
-      CheckMediocreLoaderHelper.addOldCharacterObserver();
-      CheckMediocreLoaderHelper.lockEuclideanNumberList();
-      CheckMediocreLoaderHelper.erasePermanentImpressionArray();
-      CheckMediocreLoaderHelper.prepareGeometricHeapReference();
-      CheckMediocreLoaderHelper.stopCriticalBottomPool();
-      DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-       if #available(iOS 14, *) {
-           ATTrackingManager.requestTrackingAuthorization { status in
-               // 处理授权结果
-           }
-       }
-   }
-
-      CheckMediocreLoaderHelper.offsetCustomizedTopCache();
-
+        // 初始化 Enyxoramea 模块
+    // EnyxorameaCZHomeRegister.goBack()
     GeneratedPluginRegistrant.register(with: self)
     let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
-      CheckMediocreLoaderHelper.refreshSeamlessSkewXDecorator();
-      CheckMediocreLoaderHelper.getNormalWorkflowOwner();
-      CheckMediocreLoaderHelper.enhanceNormalRotationDecorator();
-      CheckMediocreLoaderHelper.finishCurrentAllocatorBase();
-      CheckMediocreLoaderHelper.getOriginalFeaturePool();
-      CheckMediocreLoaderHelper.startNewestVariableImplement();
-      CheckMediocreLoaderHelper.analyzeCustomParameterCache();
-      CheckMediocreLoaderHelper.setActivatedDescriptionReference();
-      CheckMediocreLoaderHelper.setSortedLeftProtocol();
-      CheckMediocreLoaderHelper.rectifyEphemeralStrengthFactory();
-      CheckMediocreLoaderHelper.startDiversifiedParamHelper();
-      CheckMediocreLoaderHelper.analyzeSemanticPreviewList();
-      CheckMediocreLoaderHelper.setAccordionCycleProtocol();
-      CheckMediocreLoaderHelper.getRetainedInfrastructureCache();
-      CheckMediocreLoaderHelper.setDiscardedRendererHelper();
-      CheckMediocreLoaderHelper.pauseBasicLayoutProtocol();
-      CheckMediocreLoaderHelper.prepareDifficultFragmentsCreator();
-      CheckMediocreLoaderHelper.spinIterativeTopDecorator();
-      CheckMediocreLoaderHelper.setMutableButtonProtocol();
-    // 初始化 Enyxoramea 模块
-    EnyxorameaCZHomeRegister.goBack()
-    EnyxorameaCZHomeRegister.initEnyxorameaModule()
+           DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization { status in
+                // 处理授权结果
+            }
+        }
+    }
+
+
+    // EnyxorameaCZHomeRegister.initEnyxorameaModule()
     
     return result
   }
